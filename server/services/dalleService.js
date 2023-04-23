2d83e6d523bc651715a1a62c13ce2c7227714606
Kevin-Featherstone <117868278+Kevin-Featherstone@users.noreply.github.com>
2023-04-23 15:55:07 -0500

    refactord dalleImageController
    modularized convertStrToFileName and moved to utils folder
    modularized openAi client instantiation and moved to utils folder
    added services folder
    modularized generateImage and moved to services folder
    dalleService.generateImage now responds with base64 encoded string instead of url
    renamed dalleImagecontroller.generateImage to dalleImageController.getImageUrl
    uploadFileToS3 is now passed a buffer instead of a stream

---
+++ server/services/dalleService.js
@@ -0,0 +1,16 @@
+const openai = require('../utils/openAi');
+
+const dalleService = {};
+
+dalleService.generateImage = async (prompt) => {
+  const response = await openai.createImage({
+    prompt: prompt,
+    n: 1,
+    size: '256x256',
+    response_format: 'b64_json',
+  });
+
+  return response.data.data[0].b64_json;
+}
+
+module.exports = dalleService;
