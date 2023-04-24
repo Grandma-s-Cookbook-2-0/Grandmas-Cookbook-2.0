4ffc8c05be0a5b777047b12f0973fd16e942d87c
Nestor Cayanan <nestorcayanan@Nestors-MacBook-Air.local>
2023-04-24 13:43:53 -0500

    initial tests started

--- server/server.js
+++ server/server.js
@@ -46,3 +46,5 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+module.exports = app;
--- tests/server/controller/scrapingController.test.js
+++ tests/server/controller/scrapingController.test.js
@@ -1,8 +1,30 @@
-const supertest = require('supertest')
+const { describe } = require('node:test')
+const request = require('supertest')
+const app = require("../../../server/server.js")
 
 
-describe('sample test', () => {
-    test('test', () => {
-        expect(true).toBe(true)
-    })
-})
+describe('Scraping Integration', () => {
+   // correctly routes to the right scraper
+   describe('no url is provided', () => {
+      it('responds back a 406 with no url provided', () => {
+          request(app)
+         .get('/recipe/scrapeurl')
+         .expect(406)
+      })
+      });
+   describe('URL is not from epicurious or foodnetwork', ()=> {
+      it('responds back a 406 from a foodnetwork url', () => {
+         request(app)
+         .get('recipe/scrapeurl')
+         .query({url: 'https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chicken-and-rice-8133711' })
+         .expect(406)
+      })
+      })
+         // .query({url: 'http://www.simplyrecipes.com/recipes/banana_bread/'})
+         // .expect(406, done)
+
+      })
+   // correctly grabs the title
+   // correctly grabs the ingredient list
+   // correctly grabs the directions
+   // throws an error if incorrect url is passed
