b09b71f6fb5c1e0f74b7b361c26a2a553fca303f
Nestor Cayanan <nestorcayanan@Nestors-Air.attlocal.net>
2023-04-24 19:10:43 -0500

    commit before merging dev branch

--- tests/server/controller/scrapingController.test.js
+++ tests/server/controller/scrapingController.test.js
@@ -1,30 +1,46 @@
-const { describe } = require('node:test')
+const { describe, it, beforeEach, test } = require('node:test')
 const request = require('supertest')
 const app = require("../../../server/server.js")
-
+const server = 'http://localhost:3000';
 
 describe('Scraping Integration', () => {
    // correctly routes to the right scraper
    describe('no url is provided', () => {
-      it('responds back a 406 with no url provided', () => {
-          request(app)
+      test('responds back a 406 with no url provided', () => {
+         request(server)
          .get('/recipe/scrapeurl')
+         .query({url: '/'})
          .expect(406)
       })
       });
-   describe('URL is not from epicurious or foodnetwork', ()=> {
-      it('responds back a 406 from a foodnetwork url', () => {
-         request(app)
+   describe('URL is not from epicurious or foodnetwork', () => {
+      test('responds back a 406 from a foodnetwork url', () => {
+         request(server)
          .get('recipe/scrapeurl')
-         .query({url: 'https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chicken-and-rice-8133711' })
+         .query({url: 'https://www.simplyrecipes.com/recipes/banana_bread/' })
          .expect(406)
       })
       })
-         // .query({url: 'http://www.simplyrecipes.com/recipes/banana_bread/'})
-         // .expect(406, done)
+   // describe('foodnetwork url', () => {
+   //    // beforeAll(() => {
+   //    //    request(app)
+   //    //    .get('recipe/scrapeurl')
+   //    //    .query({url: 'https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chicken-and-rice-8133711'})
+   //    //    })
+   //    test('should grab the title', () => {
+   //       request(server)
+   //       .get('recipe/scrapeurl')
+   //       .query({url: 'https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-chicken-and-rice-8133711'})
+   //       .then((res) => {
+   //          expect(res).toBe('something')
+   //       })
+   //    }
+   // )
+   // describe('epicurious url', () => {
 
-      })
+   // })
+
+})
    // correctly grabs the title
    // correctly grabs the ingredient list
    // correctly grabs the directions
-   // throws an error if incorrect url is passed
